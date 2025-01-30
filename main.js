1a288d92b33242e24227b9ab8377b954927437ff
shakil hossain <sh9743441@gmail.com>
2025-01-30 21:54:17 +0600

    navbar

--- main.js
+++ main.js
@@ -19,6 +19,12 @@ const posts = [
         description: 'This is a post two',
         status: 'draft',
     },
+
+    {
+        title: 'post five',
+        description: 'This is a post two',
+        status: 'draft',
+    },
 ]
 
 
@@ -31,23 +37,40 @@ const archivedPost = posts.filter(function (singlePost) {
 });
 
 
-// TODO: render all draft post
-const dartPost = [];
+const draftPost = posts.filter(function (singlePost) {
+    return singlePost.status === 'draft';
+});
 
 
-const root = document.querySelector('#root');
-
+const allPost = document.querySelector('#allPost');
 let postHtml = [];
 posts.forEach(function (singlePost) {
     postHtml.push(
         `<div class="post bg-white shadow-sm p-4 rounded-md">
-          <h2>${singlePost.title}</h2>
+          <h2 class="text-lg font-semibold">${singlePost.title}</h2>
           <p>${singlePost.description}</p>
+          <p class="text-sm">Status: ${singlePost.status}</p>
       </div>`
     )
 })
 
-root.innerHTML = postHtml.join('');
+allPost.innerHTML = postHtml.join('');
+
+
+
+const draftPostHtml = document.querySelector('#draftPost');
+let draftPostHtmlArray = [];
+draftPost.forEach((singlePost) => {
+    draftPostHtmlArray.push(
+        `<div class="post bg-white shadow-sm p-4 rounded-md">
+          <h2 class="text-lg font-semibold">${singlePost.title}</h2>
+          <p>${singlePost.description}</p>
+          <p class="text-sm">Status: ${singlePost.status}</p>
+      </div>`
+    )
+})
+console.log(draftPostHtmlArray)
+draftPostHtml.innerHTML = draftPostHtmlArray.join('');
 
 
 // for (let index in posts) {
@@ -58,7 +81,7 @@ root.innerHTML = postHtml.join('');
 let result = plus(123123, 5);
 
 function plus(number1, number2) {
-   return number1 + number2;
+    return number1 + number2;
 }
 
 
